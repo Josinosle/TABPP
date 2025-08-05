e3fad986449bed2ad62dab25385a7075d8a78c88
Josinosle <137291379+Josinosle@users.noreply.github.com>
2025-08-05 22:29:15 +0100

    working max brightness set prototype

--- script.py
+++ script.py
@@ -1,8 +1,49 @@
 import dbus
 import dbus.mainloop.glib
 from gi.repository import GLib
+import os
+
+class BrightnessController:
+
+    def __init__(self):
+        self.base_path = "/sys/class/backlight"
+
+        try:
+            # Find first usable backlight device
+            device = next((d for d in os.listdir(self.base_path) if "intel" in d or "acpi" in d or "amdgpu_bl"), None)
+            if not device:
+                raise RuntimeError("No backlight device found")
+
+            self.brightness_path = os.path.join(self.base_path, device, "brightness")
+            print(f"Found backlight device: {self.brightness_path}")
+            max_path = os.path.join(self.base_path, device, "max_brightness")
+
+            with open(max_path, "r") as f:
+                self.max_brightness = int(f.read().strip())
+                print(f"Max device brightness: {self.max_brightness}")
+
+        except Exception as e:
+            print(f"Brightness device error: {e}")
+
+    def set_brightness(self,level):
+        try:
+
+            level = max(0, min(level, self.max_brightness))  # Clamp
+            with open(self.brightness_path, "w") as f:
+                f.write(str(level))
+
+            print(f"Brightness set to {level}/{self.max_brightness}")
+
+        except Exception as e:
+            print(f"Error setting brightness: {e}")
+
+
+"""
+dbus listener for fetching the power state of the device
+"""
 
 def on_properties_changed(interface, changed_properties, invalidated_properties):
+
     if 'Online' in changed_properties:
         online = changed_properties['Online']
         print(f"AC Power: {'Online' if online else 'Offline'}")
@@ -14,10 +55,15 @@ def on_properties_changed(interface, changed_properties, invalidated_properties)
             2: "Discharging",
             3: "Empty",
             4: "Fully charged",
-            5: "Pending charge",
-            6: "Pending discharge"
         }
         print(f"Battery State Changed: {state_map.get(state, 'Unknown')}")
+
+        global brightness_device_object
+
+        if state == 1 or state == 4:
+            brightness_device_object.set_brightness(1000000)
+
+
 
 def setup_listener():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -43,5 +89,7 @@ def setup_listener():
     loop.run()
 
 if __name__ == "__main__":
+    brightness_device_object = BrightnessController()
     setup_listener()
+
 
