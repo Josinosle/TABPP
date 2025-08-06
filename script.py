feca4c94129be4cf63e51544a8dc788a25ffd9d7
Josinosle <137291379+Josinosle@users.noreply.github.com>
2025-08-06 02:14:59 +0100

    double step count when moving brightness

--- script.py
+++ script.py
@@ -63,7 +63,7 @@ class BrightnessController:
 
             current = int(self.get_brightness())
             target = int(ambient_brightness * 200)
-            step_count = 10
+            step_count = 20
 
             # Prevent tiny loops or no movement
             if current == target:
@@ -75,7 +75,7 @@ class BrightnessController:
                 level = int(current + step * i)
                 level = max(0, min(level, self.max_brightness))  # Clamp
                 self.set_brightness(level)
-                time.sleep(0.1)
+                time.sleep(0.05)
 
         except Exception as e:
             print(f"Ambient brightness backlight set error: {e}")
